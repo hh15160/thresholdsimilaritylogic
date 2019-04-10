af6988857378eb883de4c33efbe6a838b7af7372
Unknown <hh15160@my.bristol.ac.uk>
2019-04-10 14:49:35 +0100

    Vary Dependence with Copula
    Framework involving Frank's Copula and a varying level of dependence.

--- VaryDependenceCopula.py
+++ VaryDependenceCopula.py
@@ -5,53 +5,29 @@ import random
 from random import randint
 import time
 import matplotlib.pyplot as plt
+from itertools import product, chain, combinations
+import math as math
 import seaborn as sns
 
 
-# Orders the similarty expressions from smallest to largest
-def sort_expressions(s):
-    s = np.array(s)
-    order = np.argsort(s)
-    #Sorts s into order smallest to largest
-    s.sort(axis=0)
-    n=np.size(s)
-    mu=np.array(np.zeros(n+1))
-    mu[0]=s[0]
-    for i in range(1, n):
-        mu[i]=s[i]-s[i-1]
-        #print(mu[i])
-    mu[n]=1-s[-1]  
-    return [s, mu, order]
 
-# Creates the truth table
-def create_truth_table(variables, formula, s):
-    variables3=[]
-    s_new = []
+def copula_truth_table(variables, formula, s):
     
-#   Only considers similarity expressions that are invloved in sentence theta
+    #   Only considers similarity expressions that are invloved in sentence theta
+    variables2=[]
     for i in range(0,len(variables)):
         if any(variables[i] in s for s in formula):
-            variables3.append(variables[i])
-            s_new.append(s[i])
-    [s, mu, order] = sort_expressions(s_new)
-    n=len(variables3)
-    variables2=[]
-    order = order[::-1]
+            variables2.append(variables[i])
+    # Now, varaibles2 contains only relevant s valuess               
     
-    
-    for i in range(0,n):
-        variables2.append(variables3[order[i]])
-    m = np.ones((n+1,n))
-    m=np.tril(m, -1)
-    rev_mu = mu[::-1]
-    m=np.asarray(m)
-    df = pd.DataFrame(m,columns= variables2)
-    df.insert(loc=0, column='S_val', value = rev_mu)
-    
+    # Generates initial 1 and 0s for truth table
+    n=len(variables2)
+    m = [i for i in product(range(2), repeat=n)] 
+    df = pd.DataFrame(m ,columns = variables2)
+
     column_names=[]
     c=0
     
-#   Loops through the entire formula
     for i in range(0,len(formula)):
         # Checks in a sub bracket
         if formula[i][0]!='or':
@@ -118,9 +94,40 @@ def create_truth_table(variables, formula, s):
             truth[i]=0
             
     df[column_name_joint]=truth
-    sval_sum = evaluation(df,column_name_joint)
-    return df, sval_sum
     
+    # Further optimisation starts here
+    # Remove rows that do not contain a 1
+    counts = df[column_name_joint].value_counts()
+    drop_value = np.argmax(counts)
+    
+    for i in range(0,len(df)):
+        
+        if df[column_name_joint][i]==drop_value:
+            df = df.drop([i])
+
+    # Need to add in the s values now
+    # Need to find the product of s's 
+    s_vals = []
+    df = df.reset_index(drop=True)
+    
+    for ii in range(0,len(df)):
+        P=[]
+        for jj in range(0, n):
+            # If equal to 1, append column name to P
+           if df[variables2[jj]][ii]==1:
+               P.append(df.columns[jj])
+           
+        x_sum = copula_atoms(P,variables2,s)
+        s_vals.append(x_sum)
+        
+    df.insert(loc=0, column='S_val', value = s_vals)
+    sval_sum = round(np.sum(df['S_val']),4)
+    
+    if drop_value == 1:
+        sval_sum = round(1-sval_sum,4)
+        
+    return df, sval_sum
+  
 
 #Evaluation function   
 def evaluation(df,column_name_joint):
@@ -133,7 +140,7 @@ def evaluation(df,column_name_joint):
     return sval_sum
 
 
-# Formula generator that generates a formula where n defines the number of similarity expressions 
+# New formula generator that generates a formula where n defines the number of similarity expressions 
 # k defines the length of the formula
 # or's are between sub_brackets, and's are in sub_brackets
 def new_formula_generator(n,k):
@@ -141,31 +148,34 @@ def new_formula_generator(n,k):
     connective_list = ['and', 'or']
     variable_list = variables
     formula=[]
-    connective=[]
     
+    connective=[]
     # Number of connectives is k-1
     for j in range(0,k-1):
         connective.append(random.choice(connective_list))
     
+    
     #Create an empty sub brakcket
     sub_bracket=[]
     
-    for i in range(0,k):   
+    for i in range(0,k):
+         
         # Decide on whether a 'not' is present or not
         if randint(0,1)==1:
             not_present=True
         else:
-            not_present=False    
+            not_present=False
+            
         s_value = random.choice(variable_list)
-        
         # Keep iterating through s_value if in formula subbracket already
         while (s_value in sub_bracket)==True:
             # Stops loop from getting stuck if k>n
-            if all(x==connective[0] for x in connective)==False:
+            if all(x==connective[0] for x in connective)==False and set(variables).issubset(sub_bracket)==False:
                 s_value = random.choice(variable_list)
             else:
                 break
-             
+            
+            
 #       Checks for 'not's
         if not_present==True:
             #if first loop through j
@@ -174,10 +184,8 @@ def new_formula_generator(n,k):
         sub_bracket.append(s_value)
         
         if i!=(k-1):
-            
             if connective[i]=='and':
                 sub_bracket.append(connective[i])
-                
             elif connective[i]=='or':
                 # Close bracket and add to formula
                 formula.append(sub_bracket)
@@ -187,100 +195,160 @@ def new_formula_generator(n,k):
                 
         else:
             formula.append(sub_bracket)
-             
+                 
     return [formula, variables, s]
             
 
-
 def create_variables(n):
-#   Initialise variables and vectors
     num_variables=n
     s=[]
     variables=[]
-    
     for i in range(0,num_variables):
         s.append(round(random.uniform(0.001, 0.999),4))
         variables.append('s'+str(i+1))
         
     return variables, s
 
+# Function for calculating Franks Copula
+def franks_copula(values):
+    alpha = 0.5
+    # Do sum first...
+    sum_values = []
+    for i in range(0, len(values)):
+        p=alpha**(values[i])-1
+        sum_values.append(p)
+    
+    prod_sum_values = np.prod(sum_values)
+    cop = math.log(1+(prod_sum_values/(alpha-1)**(len(values)-1)))/math.log(alpha)
 
+    return cop
+
+# Funciton for calculating the copula atoms    
+# Will do one atom at a time
+def copula_atoms(P,variables,s):
+    #Generate F
+    F = create_superset(P,variables)
+    x_vec = []
+    for i in range(0,len(F)):
+        values = f(variables,s,F)
+        x = (-1)**(len(F[i])-len(P)) * franks_copula(values[i])
+        x_vec.append(x)
+        
+    x_sum = np.sum(x_vec)
+    return x_sum
+
+# Function used for finding supersets
+def all_subsets(ss):
+    return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
+
+# This function works for calculating supersets of P
+# Will include empty set too
+def create_superset(P,variables): 
+    # Initialise variables
+    F=[]
+    z=[]
+    x=[]
+    
+    for subset in all_subsets(variables):
+        z.append(subset)
+
+    for i in range(len(z)):
+        x.append(list(z[i]))
+        
+    # x now contains all subsets of variables    
+    for i in range(0,len(x)):
+        if set(P).issubset(x[i]):
+            F.append(x[i])
+
+    return F
+
+# Function for calculating values for franks copula
+# Either mu(si) or 1
+def f(variables,s,F):
+    values =[]
+    for t in range(0,len(F)):
+        cop_values = []
+        for j in range(0,len(variables)):
+            if set([variables[j]]).issubset(F[t]):
+                cop_values.append(s[j])
+            else:
+                cop_values.append(1)
+        values.append(cop_values)
+    return values    
+    
 
 def analysis(n,k):
-    # Average time over 100 attempts of n or k
+    # Average time over 10 attempts of n
     time2=[]
-    
-    for p in range(0,100):
+    for p in range(0,50):
         start_time = time.time()
         formula, variables, s= new_formula_generator(n,k)
-        df, sval_sum = create_truth_table(variables, formula, s)
+        df, sval_sum = copula_truth_table(variables, formula, s)
         end_time = time.time()
         time2.append(end_time-start_time)
         
     time_taken= np.mean(time2)
-    return df, formula, sval_sum, time_taken, s
- 
-    
-# Code to run analysis once
+    return df, formula, sval_sum, time_taken
+  
+
 def run_once(n,k):
         formula, variables, s= new_formula_generator(n,k)
-        df, sval_sum = create_truth_table(variables, formula, s)
+        df, sval_sum = copula_truth_table(variables, formula, s)
         return formula, variables, df, sval_sum
+    
+
+def test():
+        formula = [['not', 's1', 'and', 's3'],['or'], ['s2']]
+        variables = ['s1','s2','s3','s4']
+        s = [0.75,0.3,0.12, 0.15]
+        df, sval_sum = copula_truth_table(variables, formula, s)
+        return formula, variables, df, sval_sum, s
+  
   
     
-# Code for testing a specific formula
-def test():
-        formula = [['not','s1','and','not','s2','and','not','s3']]
-        variables = ['s1','s2','s3','s4','s5']
-        s = [0.75,0.3,0.12, 0.33, 0.98]
-        df, sval_sum = create_truth_table(variables, formula, s)
-        return formula, variables, df, sval_sum, s    
-
-
-
 if __name__ == "__main__":    
     #Seaborn for graphs
     sns.set()
     
     
-    ###### k=15, n varied ######
+    ###### k=20, n varied ######   
     total_time=[]
-    k=15
+    k=20
     n_vec=[]
-    for n in range(1,100):
-        df, formula, sval_sum, time_taken, s = analysis((n*10)+5,k)
-        n_vec.append((n*10)+5)
+    for n in range(1,10):
+        df, formula, sval_sum, time_taken = analysis(n,k)
+        n_vec.append(n)
         total_time.append(time_taken)
         print(n)
-        
-    plt.xlabel('Number of similarity values (n)')
+    plt.xlabel('Number of Similarity Values (n)')
     plt.ylabel('Computational Time (s)')
-    plt.scatter(n_vec, total_time,marker="+") 
-    plt.plot(np.unique(n_vec), np.poly1d(np.polyfit(n_vec, total_time, 2))(np.unique(n_vec)), 'r', linewidth=3.0)
+    plt.scatter(n_vec, total_time, marker = '+') 
+    plt.plot(np.unique(n_vec), np.poly1d(np.polyfit(n_vec, total_time, 1))(np.unique(n_vec)), 'r', linewidth=3.0)
     plt.show()   
     
     ######               ######
     
+    
     # Fixed number of similarity expressions
     ###### n=1000, k varied ######
     total_time=[]
-    n=1000
+    n=10
     k_vec=[]
-    for k in range(1,100):
-        df, formula, sval_sum, time_taken, s = analysis(n,k*10)
-        k_vec.append(k*10)
+    for k in range(1,1000):
+        df, formula, sval_sum, time_taken = analysis(n,k)
+        k_vec.append(k)
         total_time.append(time_taken)
         print(k)
+        
     plt.figure()
     plt.xlabel('Length of Formula (k)')
     plt.ylabel('Computational Time (s)')
-    plt.scatter(k_vec, total_time,marker="+") 
-    plt.plot(np.unique(k_vec), np.poly1d(np.polyfit(k_vec, total_time, 2))(np.unique(k_vec)), 'r',linewidth=3.0)
+    plt.scatter(k_vec, total_time, marker="+", s=100) 
+    plt.plot(k_vec, total_time, 'r', linewidth=3)
+    #plt.plot(np.unique(k_vec), np.poly1d(np.polyfit(k_vec, total_time, 1))(np.unique(k_vec)), 'r',linewidth=3.0)
     plt.show()
-    
-    ######              ######
+        
     
     # Test a specific formula  
     formula, variables, df, sval_sum, s = test()
     
-   
